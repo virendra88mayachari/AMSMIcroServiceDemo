9455e7979ec6e01424972b676a39e2d44a03e1a5
virendra88mayachari <44014539+virendra88mayachari@users.noreply.github.com>
2018-10-11 16:40:36 -0400

    Add files via upload

---
+++ AMSMicroService2/AMSMicroService2/Controllers/CreateServicePointController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMSMicroService2.DatabaseOps;
+using AMSMicroService2.Models;
+using AMSMicroService2.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMSMicroService2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CreateServicePointController : ControllerBase
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [HttpPost]
+        public IActionResult MicroServiceServicePointGeneration(OPLD opldObject)
+        {
+            try
+            {
+                log.Info(DateTime.Now.ToString() + " AMS-POC-MicroServiceServicePointGeneration: Read OPLD message and Start processing.");
+                //CreateServicepoint                          
+                SakilaContext context = new SakilaContext("server=127.0.01;port=3306;database=sakila;user=root;password=techM@Ups1");//HttpContext.RequestServices.GetService(typeof(SakilaContext)) as SakilaContext;
+
+                log.Info(DateTime.Now.ToString() + " AMS-POC-MicroServiceServicePointGeneration: Getting OPLD tracking number matching DIALS record.");
+                //Check if opld tracking number matches with dials matching number
+                DIALS dialsObject = context.GetMatchingDialsID(opldObject.TrackingNumber);
+
+                ServicePoint servicePointObject = new ServicePoint();
+                if (dialsObject != null)
+                {
+                    servicePointObject = ServicePointUtility.CreateServicePoint(opldObject, dialsObject.ConsigneeName, dialsObject.ClarifiedSignature, true);
+                }
+                else
+                {
+                    servicePointObject = ServicePointUtility.CreateServicePoint(opldObject, "", "", false);
+                }
+
+                log.Info(DateTime.Now.ToString() + " AMS-POC-MicroServiceServicePointGeneration: Writing ServicePoint in to DB.");
+
+                //Write Servicepoint to DB
+                context.AddNewServicePoint(servicePointObject);
+            }
+            catch (Exception ex)
+            {
+                log.Error(DateTime.Now.ToString() + " AMS-POC-MicroServiceServicePointGeneration: " + Convert.ToString(ex.Message));
+                return new JsonResult(new { Result = System.Net.HttpStatusCode.InternalServerError });
+            }
+
+            return Ok(new { Result = "Success" });
+        }
+
+        public IActionResult TestMethod()
+        {
+            return Ok();
+        }
+    }
+}
